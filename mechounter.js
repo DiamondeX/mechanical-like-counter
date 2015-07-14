daa918818a8eac73cb4b7e050d917f43327fc85a
DiamondeX <DiamondeX@users.noreply.github.com>
2015-07-14 13:08:45 +0500

    reset algorythm added

--- mechounter.js
+++ mechounter.js
@@ -1,5 +1,7 @@
 (function($){
 
+  var dbgLog, adgd, dgp, dgd;
+
   //var PiD4 = 0.7853981633974483; // === Math.Pi / 4;
   var toLog10 = 0.4342944819032518; // === 1 / Math.log(10)
   var frLog10 = 2.302585092994046;  // === Math.log(10)
@@ -53,6 +55,8 @@
 
   function Mecntr(el, opts){
 
+    var self = this;
+
     this._opts = opts = $.extend({
       mask: " 3,2",
       decimalSep: ".",
@@ -62,7 +66,10 @@
       showDigitMs: 300,
       slowdownMs: 19000,
       perceptibleChangeMs: 600,
-      refreshDelayMs: 30
+      refreshDelayMs: 30,
+      resetDelayMs: 1500,
+      onBeforeSpin: function(delayMs, valueDelta, startValue){},
+      onSpinStep: function(timeFromStartMs, currentValue){}
     }, opts);
 
     if(opts.mask){
@@ -73,7 +80,21 @@
 
     this.el = el;
     this.$el = $(el).addClass(this._opts.baseClass);
-    this._height = this.$el.height();
+
+    Object.defineProperty(this, "_height", {
+      enumerable: true,
+      configurable: true,
+      get: function(){
+        var h = self.$el.height();
+        if(h <= 0) return -10000;
+        Object.defineProperty(self, "_height", {
+          value: h,
+          writable: true,
+          enumerable: true
+        });
+      }
+    });
+    //this._height = this.$el.height();
     this._digits = [];
 
     this._initDom();
@@ -155,7 +176,7 @@
 
     this._value = v;
     frac = this._digits[0].setValue(v);
-    v = Math.floor(v/10);
+    v = Math.floor(v * 0.1);
 
     while(v > 0 || d < o.digits || d < dLen){
       empty = v < 1 && d >= o.digits;
@@ -166,7 +187,7 @@
       }else{
         digit.setIntValue(v, empty);
       }
-      v = Math.floor(v/10);
+      v = Math.floor(v * 0.1);
     }
 
     this.dropEmptyDigits();
@@ -183,7 +204,7 @@
 
     while(d < dLen){
       frac = this._digits[d++].setFinal(sgn, fl, frac)._final.nextFrac;
-      fl = Math.floor(fl/10);
+      fl = Math.floor(fl * 0.1);
     }
   };
 
@@ -210,10 +231,66 @@
     return this._addDigit().setNew().setFinal(digit._sgn, fl, frac);
   };
 
+  Mecntr.prototype._calcSlowdownParams = function(aDif, delay, cb){
+    var t2 = timeByLgVal(aDif)  // time in seconds of spin slowdown
+    , t2o     // old value of "t2"
+    , t1 = 0  // time in seconds of constant spin speed
+    , k = 1   // spin slowdown intensification coefficient (initial value would be minimal)
+    , ko = k  // old value of "k"
+    , kf = 1  // fraction of modification coefficient for "k"
+    , spN = 0 // initial spin speed of spin slowdown (rounds of dif per second)
+    , sp_ = 1 // constant spin speed
+    , dt = t2 // value delta for "t2" correction
+    , i = 0   // calculation iteration index
+    ;
+
+    if(t2 > delay){
+
+      do {
+        i++;
+        t2o = t2;
+
+        k *= 1 + kf;
+        t2 = timeByLgVal(aDif, k);
+        if(t2 < delay) {
+          kf *= 0.5;
+          k = ko;
+        }else{
+          ko = k;
+        }
+      } while(t2o !== t2);
+
+      console.log(" iterations (1): ", i, ", k:", k, ", t2:", t2, ", sp:", lgSpeed(t2)*0.001);
+
+    }else{
+
+      do {
+        i++;
+        t2o = t2;
+
+        dt *= 0.5;
+        t2 += spN > sp_ ? dt : -dt;
+        t1 = delay - t2;
+        sp_ = lgSpeed(t2);
+        spN = (aDif - lgValueBySpeed(sp_)) / t1;
+      } while(t2o !== t2);
+
+      console.log(" iterations (2): ", i, ", t2:", t2, ", sp:", spN*0.001);
+
+    }
+
+    cb(t1, t2, k, spN);
+
+  };
+
   Mecntr.prototype._setSpeed = function(spN){
     this._speed = spN * 0.001;
     this._perceptibleDgt = toLog10 * Math.log(this._speed * this._opts.multiplier);
   };
+
+  Mecntr.prototype.getValue = function(){
+    return this._value / this._opts.multiplier;
+  }
 
   Mecntr.prototype.setValue = function(newVal, delayMs){
     newVal *= this._opts.multiplier;
@@ -242,61 +319,9 @@
     , t2      // time in seconds of spin slowdown
     ;
 
-    t2 = timeByLgVal(aDif);
-
-    if(t2 > delay){
-
-      //iterative calculation of parameters
-      (function(){
-        var i = 0
-        , t2o
-        , kk = 0.5
-        , ko = k
-        ;
-
-        do {
-          i++;
-          t2o = t2;
-
-          k *= 1 + kk;
-          t2 = timeByLgVal(aDif, k);
-          if(t2 < delay) {
-            kk *= 0.5;
-            k = ko;
-          }else{
-            ko = k;
-          }
-        } while(t2o !== t2);
-
-        console.log(" iterations (1): ", i, ", k:", k, ", t2:", t2, ", sp:", lgSpeed(t2)*0.001);
-      })();
-
-    }else{
-
-      //iterative calculation of parameters
-      (function(){
-        var i = 0
-        , t2o
-        , sp_ = 1
-        , mod = t2
-        ;
-
-        do {
-          i++;
-          t2o = t2;
-
-          mod *= 0.5;
-          t2 += spN > sp_ ? mod : -mod;
-          t1 = delay - t2;
-          sp_ = lgSpeed(t2);
-          spN = (aDif - lgValueBySpeed(sp_)) / t1;
-        } while(t2o !== t2);
-
-        console.log(" iterations (2): ", i, ", t2:", t2, ", sp:", spN*0.001);
-
-      })();
-
-    }
+    this._calcSlowdownParams(aDif, delay, function (t1_, t2_, k_, spN_){
+      t1 = t1_; t2 = t2_; k = k_; spN_ = spN;
+    });
 
     this._startTime = Date.now();
     this._setSpeed(spN);
@@ -321,7 +346,7 @@
 
     clearInterval(self._interval);
 
-    window.initDraw(delayMs, dif, oldVal);
+    o.onBeforeSpin(delayMs, dif, oldVal);
     self._interval = setInterval(function(){
       var t = self._elapsed();
 
@@ -332,7 +357,7 @@
       }
 
       self._currentValues(t);
-      window.draw(t, self._value);
+      o.onSpinStep(t, self._value);
 
       var v = self._value
       , dLen = self._digits.length
@@ -343,7 +368,7 @@
 
         digit = self._digits[d++] || self._appendDigit(digit);
         digit.setValue(v);
-        v = v/10;
+        v = v * 0.1;
 
       }
 
@@ -352,18 +377,126 @@
 
         digit = self._digits[d++] || self._appendDigit(digit);
         frac = digit.setValue(v, empty);
-        v = v/10;
+        v = v * 0.1;
 
         while(v >= 0.1 || d < o.digits || d < dLen){
           empty = v < 1 && d >= o.digits;
 
           digit = self._digits[d++] || self._appendDigit(digit);
           frac = digit.nearest(v, frac, empty);
-          v = v/10;
+          v = v * 0.1;
         }
       }
 
       self.dropEmptyDigits();
+
+    }, o.refreshDelayMs);
+
+  };
+
+  Mecntr.prototype.resetValue = function(newVal, delayMs){
+    this._resetValue(newVal * this._opts.multiplier, delayMs);
+  };
+
+  Mecntr.prototype._resetValue = function(newVal, delayMs){
+
+    var o = this._opts;
+
+    if(delayMs == null) delayMs = o.resetDelayMs;
+
+    if(!delayMs || this._value === newVal){
+      clearInterval(this._interval);
+      this._isResetInProgress = null;
+      this._fillValue(newVal);
+      return;
+    }
+
+    this._isResetInProgress = true;
+
+    var self = this
+    , oldVal = this._value
+    , delay = delayMs * 0.001
+    , dLen = self._digits.length
+    , maxDif = 0
+    , t1   // time in seconds of constant spin speed
+    , t2   // time in seconds of spin slowdown
+    , k    // spin slowdown intensification coefficient (k >= 1 always)
+    , spN  // initial spin speed (rounds of dif per second)
+    ;
+
+    dbgLog = window.dbgLog = {
+      adgd: []
+    };
+    adgd = dbgLog.adgd;
+
+    (function(v){
+      var dif
+      , d = 0
+      , digit
+      ;
+
+      while(d < dLen){
+        adgd.push({
+          d: d,
+          dgd: []
+        });
+        dgp = adgd[d];
+        digit = self._digits[d++];
+        dif = digit.prepareValueIntervalForReset(v);
+        if(dif > maxDif) maxDif = dif;
+        v = Math.floor(v * 0.1);
+      }
+
+      while(v > 0){
+        digit = self._addDigit(digit);
+        digit.setIntValue(v);
+        v = Math.floor(v * 0.1);
+      }
+    })(newVal);
+
+    this._calcSlowdownParams(maxDif, delay, function (t1_, t2_, k_, spN_){
+      t1 = t1_; t2 = t2_; k = k_; spN = spN_;
+    });
+
+    dbgLog.t1 = t1;
+    dbgLog.t2 = t2;
+    dbgLog.k = k;
+    dbgLog.spN = spN;
+
+    (function(){
+      var d = 0
+      , spN = lgSpeed(t2, k)
+      , v2 = lgValueBySpeed(spN, k)
+      ;
+
+      while(d < dLen){
+        dgp = adgd[d];
+        self._digits[d++].createValueUpdaterForReset(k, t2, spN, v2);
+      }
+
+    })();
+
+    this._startTime = Date.now();
+
+    clearInterval(self._interval);
+
+    self._interval = setInterval(function(){
+      var d = 0, t = self._elapsed();
+
+      if(t >= delayMs){
+        clearInterval(self._interval);
+        this._isResetInProgress = null;
+        self._fillValue(newVal);
+        return;
+      }
+
+      t *= 0.001;
+
+      while(d < dLen){
+        dgp = adgd[d];
+        dgd = dgp.dgd;
+        self._digits[d++]._updateValue(t);
+      }
 
     }, o.refreshDelayMs);
 
@@ -451,6 +584,71 @@
     });
 
   };
+
+  Digit.prototype.prepareValueIntervalForReset = function(dstVal){
+    this._srcVal = this._oldVisVal % 10;
+    this._dstVal = dstVal % 10;
+    if(this._dstVal < this._srcVal) this._dstVal += 10;
+    this._aDif = this._dstVal - this._srcVal;
+
+    dgp.src = this._srcVal;
+    dgp.dst = this._dstVal;
+    dgp.dif = this._aDif;
+
+    return this._aDif;
+  };
+
+  Digit.prototype.createValueUpdaterForReset = function(k, t2_, spN, v2){
+    var self = this
+    , t2
+    , t1 = 0
+    , delay
+    ;
+
+    if(v2 > this._aDif){
+      t2 = timeByLgVal(this._aDif, k);
+    }else{
+      t2 = t2_;
+      t1 = (this._aDif - v2) / spN;
+    }
+
+    delay = t1 + t2;
+
+    dgp.t1 = t1;
+    dgp.t2 = t2;
+    dgp.delay = delay;
+
+    this._updateValue = function(t){
+      if(t > delay){
+        dgd.push({
+          t:t,
+          n:"out"
+        });
+        self.setVisValue(self._dstVal);
+        return;
+      }
+
+      if(t <= t1){
+        dgd.push({
+          t:t,
+          mul: spN * t,
+          res: self._srcVal + spN * t,
+          n:"lin"
+        });
+        self.setVisValue(self._srcVal + spN * t);
+        return;
+      }
+
+      t -= t1;
+      dgd.push({
+        t:t,
+        mul: lgValue(t2 - t, k),
+        res: self._dstVal - lgValue(t2 - t, k),
+        n:"log"
+      });
+      self.setVisValue(self._dstVal - lgValue(t2 - t, k));
+    }
+  }
 
   Digit.prototype.setFinal = function(sgn, fl, frac){
     this._state = this.STATE_LINKED;
@@ -627,16 +825,16 @@
 
     var shift1 = -this.owner._height;
 
-    value %= 10;
+    var flDgt = this._floor % 10;
     var zero = empty ? "" : "0";
-    this.$card0.css("top",         "0px").text(value === 0 ? zero : value);
-    this.$card1.css("top", shift1 + "px").text(value === 9 ? zero : value + 1);
+    this.$card0.css("top",         "0px").text(flDgt === 0 ? zero : flDgt);
+    this.$card1.css("top", shift1 + "px").text(flDgt === 9 ? zero : flDgt + 1);
 
     this._frac = 0;
     this._fracNext = 0;
     this._oldVisVal = value;
 
-    this.empty = value === 0 && empty;
+    this.empty = flDgt === 0 && empty;
     return 0;
   };
 
